d03612b54fcdbc4b6d17136696fb0a4f573a0460
Boratt-sag <wojciechgos8@gmail.com>
2025-04-07 15:20:54 +0200

    Add files via upload

--- projekt/playerSelect.js
+++ projekt/playerSelect.js
@@ -4,29 +4,4 @@ function startCharacterSelection() {
 
   document.getElementById("character-select").style.visibility = "visible";
 }
-  const container = document.getElementById("character-container");
-  const addBtn = document.getElementById("addCharacter");
-  let characterCount = 2;
 
-  addBtn.addEventListener("click", () => {
-    if (characterCount >= 4) return;
-    characterCount++;
-
-    const newSlot = document.createElement("div");
-    newSlot.classList.add("character-slot");
-    newSlot.innerHTML = `
-                    <label>Postać ${characterCount}:</label>
-                    <select>
-                        <option>Programista</option>
-                        <option>Informatyk</option>
-                        <option>Robotyk</option>
-                        <option>Fotograf</option>
-                    </select>
-                `;
-    container.appendChild(newSlot);
-
-    if (characterCount === 4) {
-      addBtn.disabled = true;
-      addBtn.innerText = "Osiągnięto limit postaci";
-    }
-  });
