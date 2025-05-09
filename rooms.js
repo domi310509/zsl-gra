5ab2b99c1259c0c49b9a8249f646f972863df04c
ksaczy <ksaczy@proton.me>
2025-05-09 19:41:38 +0200

    Update rooms.js

--- rooms.js
+++ rooms.js
@@ -1,6 +1,6 @@
 const sale = [
   {
-    salename: "Sala 101"
+    salename: "Sala 101",
     image: "sale co xaw chciał zrobione/1 pietro/101.png",
   },
   {
@@ -13,7 +13,34 @@ const sale = [
   },
   {
     salename: "Sala 109",
-    image: "sale co xaw chciał zrobione/1 pietro/109.png,
+    image: "sale co xaw chciał zrobione/1 pietro/109.png",
   },
- 
+  {
+    salename: "sala 22",
+    image: "sale co xaw chciał zrobione/parter/22.png",
+  },
+  {
+    salename: "sala samoksztalceniowa",
+    image: "sale co xaw chciał zrobione/parter/samok.png",
+  },
+  {
+    salename: "sala 206",
+    image: "sale co xaw chciał zrobione/pietro 2/206.png",
+  },
+  {
+    salename: "sala 210",
+    image: "sale co xaw chciał zrobione/pietro 2/210.png",
+  },
+   {
+    salename: "sala 025",
+    image: "sale co xaw chciał zrobione/sale 0/025.png",
+  },
+   {
+    salename: "sala 09",
+    image: "sale co xaw chciał zrobione/sale 0/09.png",
+  },
+   {
+    salename: "sala 09a",
+    image: "sale co xaw chciał zrobione/sale 0/09a.png",
+  },
 ];
